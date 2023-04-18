ca6d94409303ce6d632d48933fedad097f9ca5f7
Cassia Sotolani <ca.sotolani@gmail.com>
2023-04-18 15:19:26 -0400

    Conhecendo os Operadores Lógicos and, or e not

---
+++ Operadores Lógicos/script.js
@@ -0,0 +1,19 @@
+/* Operadores Lógicos */
+/* E ou AND */
+/* Só é verdadeiro se ambas as partes forem verdadeiras */
+true && true === true;
+true && false === false;
+false && true === false;
+false && false === false;
+
+/* OU ou OR */
+/* Só é false se ambas as partes forem falsas */
+true || true === true;
+true || false === true;
+false || true === true;
+false || false === false;
+
+/* NÃO ou NOT */
+/* Inverte o resultado - de verdadeiro para falso e vice-versa */
+!true === false;
+!false === true;
