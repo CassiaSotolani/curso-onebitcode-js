ed25be0d06b14acf9497dd9cb8db6cfde8f77fa8
Cassia Sotolani <ca.sotolani@gmail.com>
2023-04-18 15:05:15 -0400

    Trabalhando com Camparações de dados em JS

---
+++ Comparação de Dados/script.js
@@ -0,0 +1,24 @@
+/* Comparação de Dados */
+/* Igualdade comparando valores */
+3 == '3'; // retorno é verdadeira
+
+/* Igualdade comparando valores e tipos */
+3 === '3'; // retorno é falso
+
+/* Diferente comparando valores */
+4 != '4'; // retorno é falso
+
+/* Diferente comparando valores e tipos */
+4 !== '4'; // retorno é verdadeiro
+
+/* Maior que */
+4 > 3; // retorno é verdadeiro
+
+/* Maior igual que */
+4 >= 5; // retorno é falso
+
+/* Menor que */
+3 < 2; // retorno é falso
+
+/* Menor igual que */
+5 <= 10 // retorno é verdadeiro
